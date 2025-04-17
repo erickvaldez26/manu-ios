735833da9e58a649587b269b106b59f310fecbfe
erickvaldez26 <apata26erick@hotmail.com>
2025-04-16 23:13:49 -0500

    feat: MANU-0003 implement presenters in loginFlow

--- Manu/Modules/Login/Presenters/ViewModel/LoginViewModel.swift
+++ Manu/Modules/Login/Presenters/ViewModel/LoginViewModel.swift
@@ -8,5 +8,66 @@
 import Combine
 
 class LoginViewModel: ObservableObject {
+    private let useCases: LoginUseCases
+    private var cancellables = Set<AnyCancellable>()
     
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var errorMessage: String? = nil
+    @Published var loginSuccess: Bool = false
+    @Published var isLoading: Bool = false
+    
+    var isEmailValid: Bool = false
+    var isPasswordValid: Bool = false
+    var isFormValid: MButtonState {
+        if isEmailValid && isPasswordValid {
+            return .enabled
+        } else {
+            return .disabled
+        }
+    }
+    
+    init(useCases: LoginUseCases) {
+        self.useCases = useCases
+    }
+    
+    func login() {
+        isLoading = true
+        useCases.signInWithEmail(email: email, password: password)
+            .sink { result in
+                self.isLoading = false
+                switch result {
+                case .success:
+                    print("MANU: Se inicio sesion correctamente")
+                    self.loginSuccess = true
+                case .failure:
+                    print("MANU: Fallo el login")
+                    self.loginSuccess = false
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    func validateEmail() {
+        if email.isEmpty {
+            isEmailValid = false
+            errorMessage = "El campo de correo no puede estar vacío."
+        } else if !email.isValidEmail() {
+            isEmailValid = false
+            errorMessage = "El correo ingresado no parece válido. Asegúrate de usar el formato 'nombre@dominio.com'."
+        } else {
+            isEmailValid = true
+            errorMessage = nil
+        }
+    }
+    
+    func validatePassword() {
+        if password.isEmpty {
+            isPasswordValid = false
+            errorMessage = "Debes ingresar una contraseña para continuar."
+        } else {
+            isPasswordValid = true
+            errorMessage = nil
+        }
+    }
 }
